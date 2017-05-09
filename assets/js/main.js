883633bb075277688b4907bc2864564ce54573c6
marcelaOrellana <marcelapaz.1985@gmail.com>
2017-05-09 13:30:16 -0300

    add student en js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,24 @@
 //Constructor:
 
+var estudiantes = [];
+
 function Students(nombre, porcentajeTecnico, porcentajeHSC){
-    this.nombre = nombre;
-    this.porcentajeTecnico = porcentajeTecnico;
-    this.porcentajeHSC = porcentajeHSC;
+	this.nombre = nombre;
+	this.porcentajeTecnico = porcentajeTecnico;
+	this.porcentajeHSC = porcentajeHSC;
+}
+
+function addStudent(){
+	var nombreStudent = prompt("nombre de estudianta");
+	var porcentajeTecnicoStudent = prompt("Porcentaje Técnico");
+	var porcentajeHSCStudent = prompt("Porcentaje habilidades socio emocional");
+
+	var miDiv = document.getElementById("mostrar-pantalla");
+	var parrafo1 = document.createElement("p"); 
+	
+	var estudiante = new Students(nombreStudent, porcentajeTecnicoStudent, porcentajeHSCStudent);
+	estudiantes.push(estudiante);
+
+	parrafo1.innerHTML = "Nombre: " + nombreStudent + "<br>porcentaje técnico: " + porcentajeTecnicoStudent + "<br>Porcentaje HSC: " + porcentajeHSCStudent;
+	miDiv.appendChild(parrafo1);
 }
